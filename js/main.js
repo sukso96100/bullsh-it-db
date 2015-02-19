fa6297ca736f1a00b4d2d0f77d8073b88d960f69
sukso96100 <sukso96100@gmail.com>
2015-02-20 01:20:49 +0900

    update

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ var userip;
 
 function searchBullshIT(){
     if (event.keyCode == 13){
+        var input = document.getElementById("findnum").value;
+        console.log(input)
     var BullshITBankDB = Parse.Object.extend("BullshITBankDB");
     var query = new BullshITBankDB();
-            query.equalTo("username", CurrentUserVar.getUsername());
+            query.equalTo("phone", input);
             query.find({
               success: function(results) {
                 for (var i = 0; i < results.length; i++) {
