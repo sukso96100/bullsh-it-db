eecf6cc7e36e987f0c659a0cc4e26750b929c522
sukso96100 <sukso96100@gmail.com>
2015-02-20 22:37:10 +0900

    update

--- js/detail.js
+++ js/detail.js
@@ -7,15 +7,10 @@ function showDetail(element){
             query.first({
                 success: function(results) {
                     toggleDetails(true)
-//                    
-//                        var object = results[i];
-//                        var phone = object.get('phone');
-//                        var submitterip = object.get('submitterip');
-//                        var element = '<paper-item onclick="showDetail("'+phone+'">'+phone+'</paper-item>'
-//                        $('#results').append(element);
                     
                         document.getElementById("details-number").innerHTML = number;
                         document.getElementById("submitter-guid").innerHTML = "등룩한 사람 : " + results.get("submitterguid");
+                        document.getElementById("bullshitcount").innerHTML = results.get("bullshitcount").length;
               },
                 error: function(error) {
                     showToast("번호가 없습니다.")
@@ -31,7 +26,45 @@ function toggleDetails(boolean){
 }
 
 function updateCount(){
+    var number = document.getElementById("details-number").innerHTML;
+    
+    var BullshITBankDB = Parse.Object.extend("BullshITBankDB");
+        var query = new Parse.Query(BullshITBankDB);
+            query.equalTo("phone", number);
+            query.first({
+                success: function(results) {
+                    toggleDetails(true)
+                    
+                        var array = results.get("bullshitcount")
+                        var index = array.indexOf(guid);
+                        if (index > -1) {
+                            array.splice(index, 1);
+                            }else{
+                            array.push(guid)
+                            }
+                    document.getElementById("details-number").innerHTML = array.length;
+                        results.set("bullshitcount",array)
+                        results.save(null, {
+                        success: function(userIntroData) {  
+                            },
+                        error: function(userIntroData, error) {
+                              }
+                            });
+              },
+                error: function(error) {
+                    showToast("번호가 없습니다.")
+                    //Data Not Exist
+//                     document.getElementById('statemsg').innerHTML = "";
+                        }
+                    });
 }
 
 function updateComment(){
 }
+
+//function searchStringInArray (str, strArray) {
+//    for (var j=0; j<strArray.length; j++) {
+//        if (strArray[j].match(str)) return j;
+//    }
+//    return -1;
+//}
