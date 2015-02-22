a5e6d27913f4bd6dab9dad3bc8613fbc1ff2fa26
sukso96100 <sukso96100@gmail.com>
2015-02-22 19:53:54 +0900

    added confirm dialog

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function searchBullshIT(){
 
 function addBullshIT(){
     if (event.keyCode == 13){
-     addBullshITTask()
+     addBullshITtask()
         }
 }
 
@@ -59,14 +59,9 @@ function duplicationCheck(){
                     if(results !=undefined){
                     showToast("이미 저장된 번호입니다.")
                     }else{
-                        var BullshITBankDB = Parse.Object.extend("BullshITBankDB");
-                        var bullshITBankDB = new BullshITBankDB();
-                        bullshITBankDB.set("phone",input.toString())
-                        bullshITBankDB.set("submitterguid",guid)
-                        bullshITBankDB.save().then(function(object) {
-                        showToast("입력하신 번호가 저장되었습니다. : "+input.toString());
-    });
+                        showConfirmDialog()
                     }
+                      
               },
                 error: function(error) {
                     showToast("번호가 없습니다.")
@@ -75,3 +70,64 @@ function duplicationCheck(){
                         }
                     });
 }
+
+
+function showConfirmDialog(){
+    var input = document.getElementById("newnum").value;
+    var confirm_phone = document.getElementById("confirm_phone");
+    var confirm_guid = document.getElementById("confirm_guid");
+    
+    confirm_phone.innerHTML = input;
+    confirm_guid.innerHTML = "[등록자]"+guid;
+    
+    document.getElementById("confirm_dialog").opened = true;
+}
+
+function addNewOneFinally(){
+    setTimeout(function(){
+    var input = document.getElementById("newnum").value;
+    var confirm_comment = document.getElementById("confirm_comment_input").value;
+    if(confirm_comment.length<1){
+        showToast("설명을 입력하지 않으시면 등록되지 않습니다.")
+    }else{
+         var BullshITBankDB = Parse.Object.extend("BullshITBankDB");
+    var bullshITBankDB = new BullshITBankDB();
+    bullshITBankDB.set("phone",input.toString())
+    bullshITBankDB.set("submitterguid",guid)
+    bullshITBankDB.save().then(function(object) {
+    showToast("입력하신 번호가 저장되었습니다. : "+input.toString());
+        addCommentForNewOne(confirm_comment)
+    
+ });
+}
+        }, 1000);
+}
+                               
+function addCommentForNewOne(confirm_comment){
+         var inputi = document.getElementById("newnum").value;
+    var BullshITBankDB = Parse.Object.extend("BullshITBankDB");
+        var query = new Parse.Query(BullshITBankDB);
+            query.equalTo("phone", inputi.toString);
+            query.first({
+                success: function(results) {
+                    var array = results.get("comments")
+                    if(array == null || array == undefined){
+                        array = new Array();
+                        }
+                        if(newcomment == "" || (newcomment == null || newcomment == undefined)){}else{
+                            array.push(confirm_comment+"["+guid+"]");
+                        }
+                        results.set("comments",array)
+                        results.save(null, {
+                        success: function(Data) {  
+                            },
+                        error: function(Data, error) {
+                              }
+                            });
+              },
+                error: function(error) {
+                    //Data Not Exist
+//                     document.getElementById('statemsg').innerHTML = "";
+                        }
+                    });   
+    }
